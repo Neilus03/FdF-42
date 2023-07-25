13d25d52c83764d8d56b213056bd5946f7014f3c
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-07-25 10:33:33 +0200

    initial .h

---
+++ FdF.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FdF.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-la-f <nde-la-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 10:25:11 by nde-la-f          #+#    #+#             */
+/*   Updated: 2023/07/25 10:33:18 by nde-la-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <mlx.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+#endif
